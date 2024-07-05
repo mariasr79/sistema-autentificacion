5b07abd9dd4b822ef3f21616e3bc625fa2c87acf
mariasr79 <147938132+mariasr79@users.noreply.github.com>
2024-07-05 16:40:25 +0000

    finalizado

--- src/api/routes.py
+++ src/api/routes.py
@@ -6,9 +6,14 @@ from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+
 api = Blueprint('api', __name__)
 
-# Allow CORS requests to this API
+
 CORS(api)
 
 
@@ -16,7 +21,60 @@ CORS(api)
 def handle_hello():
 
     response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+        "message": "Hola has accedido correctamente al endpoint"
     }
 
     return jsonify(response_body), 200
+
+
+
+@api.route("/login", methods=["POST"])
+def login():
+
+
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+
+    user = User.query.filter_by(email=email).first()
+
+
+    if user is None:
+        return jsonify({"msg": "email incorrecto"}), 401
+
+
+    if password != user.password:
+        return jsonify({"msg": "contraseña incorrecta"}), 401
+
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
+
+
+
+
+
+
+@api.route("/signup", methods=["POST"])
+def signup():
+
+
+    body = request.get_json()
+
+
+    user = User.query.filter_by(email=body["email"]).first()
+
+
+    if user is None:
+
+        user = User(email=body["email"], password=body["password"], is_active=True)
+
+        db.session.add(user)
+        db.session.commit()
+
+
+        return jsonify({"msg":"Usuario creado correctamente"}) , 200
+    
+    else:
+
+        return jsonify({"msg":"Ese email ya está registrado"}) , 401
---
+++ src/front/js/component/Signup.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+
+const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const dataToSend = { email, password };
+    
+    try {
+        const response = await fetch ("https://verbose-goldfish-5gqvx4jr5xj9cp794-3001.app.github.dev/api/signup" , {
+            method:"POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(dataToSend),
+        });
+
+        const responseData = await response.json();
+
+        if (response.ok) {
+            alert("Usuario creado correctamente");
+
+        } else {
+            alert("Error al crear el usuario");
+        }
+    } catch (error) {
+        console.error ("Error al realizar la solicitud")
+    }
+};
+
+
+
+  return (
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-body">
+              <h2 className="card-title text-center mb-3 display-5">
+                Registro
+              </h2>
+              <form onSubmit={handleSubmit}>
+                <div className="form-group mt-3 h6">
+                  <label htmlFor="email" className="mb-1">
+                    Correo electrónico:
+                  </label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    value={email}
+                    onChange={handleEmailChange}
+                    required
+                  />
+                </div>
+                <div className="form-group mt-3 h6">
+                  <label htmlFor="password" className="mb-1">
+                    Contraseña:
+                  </label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                    required
+                  />
+                </div>
+                <div className="text-center">
+                  <button type="submit" className="btn btn-primary mt-5">
+                    Registrarse
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
