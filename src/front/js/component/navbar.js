6f1a85ed02f8dc721f0509bca9bbe0a37d629305
mariasr79 <147938132+mariasr79@users.noreply.github.com>
2024-07-12 08:01:03 +0000

    renderizado condicional del logout

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+	useEffect(() => {
+		// Comprobamos si el token existe en localStorage al cargar la página
+		if (localStorage.getItem("token")) {
+			setIsAuthenticated(true);
+		}
+	}, []);
+
 	const logout = () => {
 		localStorage.removeItem("token");
+		setIsAuthenticated(false);
 	};
+
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
@@ -12,15 +23,21 @@ export const Navbar = () => {
 					<span className="navbar-brand mb-0 h1">Inicio</span>
 				</Link>
 				<div className="ml-auto">
-					<Link to="/signup">
-						<button className="btn btn-primary m-1 ">Signup</button>
-					</Link>
-					<Link to="/login">
-						<button className="btn btn-primary m-1">Login</button>
-					</Link>
-					<Link to="/">
-						<button className="btn btn-primary m-1" onClick={() => logout()}>Logout</button>
-					</Link>
+					{!isAuthenticated && (
+						<>
+							<Link to="/signup">
+								<button className="btn btn-primary m-1">Signup</button>
+							</Link>
+							<Link to="/login">
+								<button className="btn btn-primary m-1">Login</button>
+							</Link>
+						</>
+					)}
+					{isAuthenticated && (
+						<Link to="/">
+							<button className="btn btn-primary m-1" onClick={logout}>Logout</button>
+						</Link>
+					)}
 				</div>
 			</div>
 		</nav>
